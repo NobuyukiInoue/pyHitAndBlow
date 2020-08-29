b8d14d160088a95497a441ccd67c3cda55c83454
Nobuyuki Inoue <gx3n-inue@asahi-net.or.jp>
2020-08-30 00:49:05 +0900

    add comments.

--- pyHitAndBlow.py
+++ pyHitAndBlow.py
@@ -6,13 +6,19 @@ import random
 import sys
 import time
 
-class TargetArray:
+class TargetNumber:
+    """
+    TargetNumber class.
+    """
     def __init__(self, number, enabled):
         self.number = number
         self.enabled = enabled
 
 
 class History:
+    """
+    Answer storage class.
+    """
     def __init__(self):
         self.challenge = []
         self.response = []
@@ -20,6 +26,9 @@ class History:
 
 
 def main():
+    """
+    function main.
+    """
     enable_print = False
     if len(sys.argv) >= 2:
          if sys.argv[1].upper() == "TRUE":
@@ -32,43 +41,49 @@ def main():
             print("debug_mode ... True")
             print("set answer ... {0}".format(answer_number))
 
-    result_table = create_table(4)
+    target_numbers = create_target_numbers(4)
+    calc(4, target_numbers, enable_print, answer_number)
 
-#   print_and_count_result(result_table, True)
+def create_target_numbers(n:int)-> list:
+    """
+    create target numbers.
+    """
+    target_numbers = []
 
-    calc(4, result_table, enable_print, answer_number)
-
-def create_table(n):
-    result_table = []
-
-    def sub_create_table(n, workStr):
+    def sub_create_target_numbers(n, workStr):
         if n == 0:
-            result_table.append(TargetArray(workStr, True))
+            target_numbers.append(TargetNumber(workStr, True))
             return
         for i in range(0, 10):
             if str(i) not in workStr:
-                sub_create_table(n - 1, workStr + str(i))
+                sub_create_target_numbers(n - 1, workStr + str(i))
 
     if n == 1:
         for i in range(0, 10):
-            result_table.append(TargetArray(str(i), True))
+            target_numbers.append(TargetNumber(str(i), True))
     
     elif n > 1:
         for i in range(0, 10):
-            sub_create_table(n - 1, str(i))
+            sub_create_target_numbers(n - 1, str(i))
 
-    return result_table
+    return target_numbers
 
-def calc(n, result_table, enable_print, answer_number):
+def calc(n:int, target_numbers:str, enable_print:bool, answer_number:str)-> bool:
+    """
+    caluculate Your number.
+    """
     H, B = 0, 0
     challenge_count = 0
     history = History()
 
     while H < n:
+        """
+        remaining count check.
+        """
         if answer_number == "":
-            remainig_count = print_and_count_result(result_table, enable_print, "")
+            remainig_count = print_and_count_remaining(target_numbers, enable_print, "")
         else:
-            remainig_count = print_and_count_result(result_table, enable_print, answer_number)
+            remainig_count = print_and_count_remaining(target_numbers, enable_print, answer_number)
 
         history.remaining_count.append(remainig_count)
 
@@ -80,11 +95,11 @@ def calc(n, result_table, enable_print, answer_number):
 
         # print canidiate number.
         while True:
-            num = int(random.random() * len(result_table))
-            target_number = str(result_table[num].number)
-            if result_table[num].enabled and target_number not in history.challenge:
+            index = int(random.random() * len(target_numbers))
+            target_number = str(target_numbers[index].number)
+            if target_numbers[index].enabled and target_number not in history.challenge:
                 history.challenge.append(target_number)
-                print("Your number is {0} ?".format(target_number))
+                print("Is your number {0} ?".format(target_number))
                 break
 
         # input response.
@@ -92,7 +107,7 @@ def calc(n, result_table, enable_print, answer_number):
             H, B = response_check(n, answer_number, target_number)
         else:
             while True:
-                print("[{0:2d}] : ".format(challenge_count), end = "")
+                print("[{0:d}] : ".format(challenge_count), end = "")
                 workStr = input("please input H, B = ")
                 flds = workStr.replace(" ", "").split(",")
                 if len(flds) == 2:
@@ -112,17 +127,22 @@ def calc(n, result_table, enable_print, answer_number):
 
         history.response.append([H, B])
 
-        for i in range(len(result_table)):
-            if result_table[i].enabled == False:
+        for i in range(len(target_numbers)):
+            if target_numbers[i].enabled == False:
+                # continue if not a candidate.
                 continue
 
-            if answer_check(n, result_table[i].number, target_number, H, B) == False:
-                result_table[i].enabled = False
+            if answer_check(n, target_numbers[i].number, target_number, H, B) == False:
+                # Remove from candidates.
+                target_numbers[i].enabled = False
 
     print_history(history, True)
 
 
-def response_check(n, answer_number, target_number):
+def response_check(n:int, answer_number:str, target_number:str):
+    """
+    response check.
+    """
     H, B = 0, 0
     for col in range(0, n):
         if target_number[col] == answer_number[col]:
@@ -135,7 +155,10 @@ def response_check(n, answer_number, target_number):
 
     return H, B
 
-def answer_check(n, table_number, target_number, H, B):
+def answer_check(n:int, table_number:str, target_number:str, H:int, B:int):
+    """
+    answer check.
+    """
     check_H, check_B = 0, 0
 
     for col in range(0, n):
@@ -156,15 +179,18 @@ def answer_check(n, table_number, target_number, H, B):
     return True
 
 
-def print_and_count_result(result_table, enable_print, answer_number):
+def print_and_count_remaining(target_numbers:list, enable_print:bool, answer_number:str):
+    """
+    print and count remaining.
+    """
     remaing_count = 0
     is_left_answer = False
-    for i in range(len(result_table)):
-        if result_table[i].enabled == True:
+    for i in range(len(target_numbers)):
+        if target_numbers[i].enabled == True:
             remaing_count += 1
             if enable_print:
-                print("{0:04}: ({1}, {2})".format(remaing_count, result_table[i].number, result_table[i].enabled))
-            if result_table[i].number == answer_number:
+                print("{0:04}: ({1}, {2})".format(remaing_count, target_numbers[i].number, target_numbers[i].enabled))
+            if target_numbers[i].number == answer_number:
                 is_left_answer = True
 
     if answer_number != "":
@@ -179,7 +205,10 @@ def print_and_count_result(result_table, enable_print, answer_number):
     return remaing_count
 
 
-def print_history(history, result):
+def print_history(history:History, result:bool):
+    """
+    print history.
+    """
     if result:
         print("calculate successful.")
     else:
@@ -187,7 +216,7 @@ def print_history(history, result):
 
     print("\n===== challenge history =====")
     for i in range(len(history.challenge)):
-        print("{0} ({1:4d}) <--- {2} ({3}, {4})".format(i + 1, history.remaining_count[i], history.challenge[i], history.response[i][0], history.response[i][1]))
+        print("[{0}] ({1:4d}) <--- {2} ({3}, {4})".format(i + 1, history.remaining_count[i], history.challenge[i], history.response[i][0], history.response[i][1]))
 
     return True
 

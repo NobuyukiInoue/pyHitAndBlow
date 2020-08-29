d580f22f4fe3311cd9367174482deff98ad18a8c
Nobuyuki Inoue <gx3n-inue@asahi-net.or.jp>
2020-08-29 23:50:33 +0900

    modify create_table() and blow count check.

--- pyHitAndBlow.py
+++ pyHitAndBlow.py
@@ -11,6 +11,14 @@ class TargetArray:
         self.number = number
         self.enabled = enabled
 
+
+class History:
+    def __init__(self):
+        self.challenge = []
+        self.response = []
+        self.remaining_count = []
+
+
 def main():
     enable_print = False
     if len(sys.argv) >= 2:
@@ -24,39 +32,39 @@ def main():
             print("debug_mode ... True")
             print("set answer ... {0}".format(answer_number))
 
-    result_table = create_table()
+    result_table = create_table(4)
 
 #   print_and_count_result(result_table, True)
 
-    calc(result_table, enable_print, answer_number)
+    calc(4, result_table, enable_print, answer_number)
 
-def create_table():
+def create_table(n):
     result_table = []
-    for n1 in range(0, 10):
-        for n2 in range(0, 10):
-            if n2 == n1:
-                continue
-            for n3 in range(0, 10):
-                if n3 == n2 or n3 == n1:
-                    continue
-                for n4 in range(0, 10):
-                    if n4 == n3 or n4 == n2 or n4 == n1:
-                        continue
-                    result_table.append(TargetArray(str(n1) + str(n2) + str(n3) + str(n4), True))
+
+    def sub_create_table(n, workStr):
+        if n == 0:
+            result_table.append(TargetArray(workStr, True))
+            return
+        for i in range(0, 10):
+            if str(i) not in workStr:
+                sub_create_table(n - 1, workStr + str(i))
+
+    if n == 1:
+        for i in range(0, 10):
+            result_table.append(TargetArray(str(i), True))
+    
+    elif n > 1:
+        for i in range(0, 10):
+            sub_create_table(n - 1, str(i))
+
     return result_table
 
-class History:
-    def __init__(self):
-        self.challenge = []
-        self.response = []
-        self.remaining_count = []
-
-def calc(result_table, enable_print, answer_number):
+def calc(n, result_table, enable_print, answer_number):
     H, B = 0, 0
     challenge_count = 0
     history = History()
 
-    while H < 4:
+    while H < n:
         if answer_number == "":
             remainig_count = print_and_count_result(result_table, enable_print, "")
         else:
@@ -72,38 +80,35 @@ def calc(result_table, enable_print, answer_number):
 
         # print canidiate number.
         while True:
-            n = int(random.random() * len(result_table))
-            target_number = str(result_table[n].number)
-            if result_table[n].enabled and target_number not in history.challenge:
+            num = int(random.random() * len(result_table))
+            target_number = str(result_table[num].number)
+            if result_table[num].enabled and target_number not in history.challenge:
                 history.challenge.append(target_number)
                 print("target is {0} ?".format(target_number))
                 break
 
         # input response.
-        while True:
-            print("[{0:2d}] : ".format(challenge_count), end = "")
-            workStr = input("please input H, B = ")
-            flds = workStr.replace(" ", "").split(",")
-            if len(flds) == 2:
-                try:
-                    H, B = int(flds[0]), int(flds[1])
-                    if H < 0 or H > 4:
+        if answer_number != "":
+            H, B = response_check(n, answer_number, target_number)
+        else:
+            while True:
+                print("[{0:2d}] : ".format(challenge_count), end = "")
+                workStr = input("please input H, B = ")
+                flds = workStr.replace(" ", "").split(",")
+                if len(flds) == 2:
+                    try:
+                        H, B = int(flds[0]), int(flds[1])
+                        if H < 0 or H > n:
+                            continue
+                        if B < 0 or B > n:
+                            continue
+                        if H == n - 1 and B == 1:
+                            continue
+                        break
+                    except:
                         continue
-                    if B < 0 or B > 4:
-                        continue
-                    if H == 3 and B == 1:
-                        continue
-                    break
-                except:
-                    continue
 
-            print("input response is Hit = {0}, Blow = {1}".format(H, B))
-            if answer_number != "":
-                check_H, check_B = response_check(answer_number, target_number)
-                if not (check_H == H and check_B == B):
-                    print("response check Error!! collect is ({0}, {1})".format(check_H, check_B))
-                    continue
-                break
+        print("input response is Hit = {0}, Blow = {1}".format(H, B))
 
         history.response.append([H, B])
 
@@ -111,68 +116,39 @@ def calc(result_table, enable_print, answer_number):
             if result_table[i].enabled == False:
                 continue
 
-            if answer_check(result_table[i].number, target_number, H, B) == False:
+            if answer_check(n, result_table[i].number, target_number, H, B) == False:
                 result_table[i].enabled = False
 
     print_history(history, True)
 
 
-def response_check(answer_number, target_number):
+def response_check(n, answer_number, target_number):
     H, B = 0, 0
-    for col in range(0, 4):
+    for col in range(0, n):
         if target_number[col] == answer_number[col]:
             H += 1
 
-    if target_number[0] == answer_number[1] \
-    or target_number[0] == answer_number[2] \
-    or target_number[0] == answer_number[3]:
-        B += 1
-
-    if target_number[1] == answer_number[0] \
-    or target_number[1] == answer_number[2] \
-    or target_number[1] == answer_number[3]:
-        B += 1
-
-    if target_number[2] == answer_number[0] \
-    or target_number[2] == answer_number[1] \
-    or target_number[2] == answer_number[3]:
-        B += 1
-
-    if target_number[3] == answer_number[0] \
-    or target_number[3] == answer_number[1] \
-    or target_number[3] == answer_number[2]:
-        B += 1
+    for i in range(0, n):
+        for j in range(0, n):
+            if i != j and target_number[i] == answer_number[j]:
+                B += 1
 
     return H, B
 
-def answer_check(table_number, target_number, H, B):
+def answer_check(n, table_number, target_number, H, B):
     check_H, check_B = 0, 0
-    for col in range(0, 4):
+
+    for col in range(0, n):
         if target_number[col] == table_number[col]:
             check_H += 1
     
     if check_H != H:
        return False
 
-    if target_number[0] == table_number[1] \
-    or target_number[0] == table_number[2] \
-    or target_number[0] == table_number[3]:
-        check_B += 1
-
-    if target_number[1] == table_number[0] \
-    or target_number[1] == table_number[2] \
-    or target_number[1] == table_number[3]:
-        check_B += 1
-
-    if target_number[2] == table_number[0] \
-    or target_number[2] == table_number[1] \
-    or target_number[2] == table_number[3]:
-        check_B += 1
-
-    if target_number[3] == table_number[0] \
-    or target_number[3] == table_number[1] \
-    or target_number[3] == table_number[2]:
-        check_B += 1
+    for i in range(0, n):
+        for j in range(0, n):
+            if i != j and target_number[i] == table_number[j]:
+                check_B += 1
 
     if check_B != B:
         return False
